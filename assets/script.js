b59592fe6821d1975075e966174bee4a0104c42e
Neil Webfactories <neiljunsan09@gmail.com>
2026-09-24 22:17:54 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 const toggle=document.querySelector('.menu-toggle');const nav=document.querySelector('.nav');if(toggle){toggle.addEventListener('click',()=>nav.classList.toggle('open'));}
+document.addEventListener("mousemove", function (e) {
+    document.documentElement.style.setProperty(
+        "--mouse-x",
+        e.clientX + "px"
+    );
+
+    document.documentElement.style.setProperty(
+        "--mouse-y",
+        e.clientY + "px"
+    );
+});
